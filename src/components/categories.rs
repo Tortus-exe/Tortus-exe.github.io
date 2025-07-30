aa8101681f393b0a4dadc09cd2570ed7dcc94ccc
Tortus-exe <65506144+Tortus-exe@users.noreply.github.com>
2025-07-30 09:31:29 -0700

    make the categories display the right categories of contents

--- src/components/categories.rs
+++ src/components/categories.rs
@@ -1,7 +1,8 @@
 use dioxus::prelude::*;
 use std::str::FromStr;
+use crate::Route;
 
-#[derive(PartialEq, Clone, Debug)]
+#[derive(PartialEq, Clone, Debug, Copy)]
 pub enum BlogCategory {
     BQN,
     Cantonese
@@ -36,10 +37,67 @@ impl std::fmt::Display for ParseCategoryError {
     }
 }
 
-#[component]
-pub fn Category(category: BlogCategory) -> Element {
-    rsx! {
-        "this will list all things under the {category.to_string()} Category."
+#[derive(Clone, PartialEq)]
+struct BlogEntry {
+    filename: &'static str,
+    title: &'static str,
+}
+
+const bqn_categories: [BlogEntry; 1] = [
+    BlogEntry {filename: "introduction.md", title: "introduction"},
+];
+
+const cantonese_categories: [BlogEntry; 6] = [
+    BlogEntry {filename: "phonetics.md", title: "phonetics"},
+    BlogEntry {filename: "sentences.md", title: "sentences"},
+    BlogEntry {filename: "time_and_tense.md", title: "time and tense"},
+    BlogEntry {filename: "questions_and_answers.md", title: "questions and answers"},
+    BlogEntry {filename: "orders.md", title: "orders, orders!"},
+    BlogEntry {filename: "numbers.md", title: "from one to infinity"},
+];
+
+fn GetBlogList(category: &BlogCategory) -> Box<[BlogEntry]> {
+    match category {
+        BlogCategory::BQN => Box::new(bqn_categories),
+        BlogCategory::Cantonese => Box::new(cantonese_categories),
     }
 }
 
+#[component]
+pub fn Category(category: BlogCategory) -> Element {
+    let blog_list = GetBlogList(&category);
+
+    rsx! {
+        div { id: "category_listing",
+            div {
+                for (num, b) in blog_list.into_iter().enumerate() {
+                    CategoryButton { 
+                        blogCategory: category,
+                        blogEntry: b.clone(), 
+                        num: num 
+                    }
+                }
+            }
+            div {
+                
+            }
+        }
+    }
+}
+
+#[component]
+pub fn CategoryButton(blogCategory: BlogCategory, blogEntry: BlogEntry, num: usize) -> Element {
+    let nav = navigator();
+    // let title = String::from(blogElem.title).clone();
+    let title = blogEntry.title;
+
+    rsx! {
+        button { id: "category_button",
+            onclick: move |_| { nav.push(Route::Blog { 
+                category: blogCategory,
+                name: title.to_string(),
+            }); },
+            "{num} - {title}"
+        }
+    }
+}
